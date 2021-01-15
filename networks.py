db531f0e686ab2ddde89ae8f166e24e5e758b157
linchenguo <linchenguo0622@qq.com>
2021-01-15 22:36:48 +0800

    add secure key for networks

--- networks.py
+++ networks.py
@@ -32,7 +32,7 @@ def get_norm_layer(norm_type='batch'):
 
 class UnetGenerator(nn.Module):
     """Create a Unet-based hiding network."""
-    def __init__(self, input_nc, output_nc, num_downs, nhf=64, norm_type='none', use_dropout=False, output_function='sigmoid'):
+    def __init__(self, input_nc, output_nc, num_downs, nhf=64, norm_type='none', use_dropout=False, output_function='sigmoid', key_len=None, redundance_size=None):
         """Construct a Unet generator.
 
         Parameters:
@@ -44,6 +44,8 @@ class UnetGenerator(nn.Module):
             norm_type (str)       -- normalization layer type
             use_dropout (bool)    -- if use dropout layers
             output_function (str) -- activation function for the outmost layer [sigmoid | tanh]
+            key_len (int)         -- length of secure key (`None` denotes no key)
+            redundance_size (int) -- redundance size for secure key by a fully connected layer
 
         We construct the U-Net from the innermost layer to the outermost layer.
         It is a recursive process.
@@ -60,7 +62,7 @@ class UnetGenerator(nn.Module):
         unet_block = UnetSkipConnectionBlock(nhf*4, nhf*8, input_nc=None, submodule=unet_block, norm_layer=norm_layer)
         unet_block = UnetSkipConnectionBlock(nhf*2, nhf*4, input_nc=None, submodule=unet_block, norm_layer=norm_layer)
         unet_block = UnetSkipConnectionBlock(nhf, nhf*2, input_nc=None, submodule=unet_block, norm_layer=norm_layer)
-        self.model = UnetSkipConnectionBlock(output_nc, nhf, input_nc=input_nc, submodule=unet_block, outermost=True, norm_layer=norm_layer, output_function=output_function)
+        self.model = UnetSkipConnectionBlock(output_nc, nhf, input_nc=input_nc, submodule=unet_block, outermost=True, norm_layer=norm_layer, output_function=output_function, key_len=key_len, redundance_size=redundance_size)
 
         if output_function == 'tanh':
             self.factor = 10 / 255  # by referencing the engineering choice in universal adversarial perturbations
@@ -69,14 +71,14 @@ class UnetGenerator(nn.Module):
         else:
             raise NotImplementedError('activation funciton [%s] is not found' % output_function)
 
-    def forward(self, X):
+    def forward(self, X, k=None):
         """standard forward."""
-        return self.factor * self.model(X)
+        return self.factor * self.model(X, k)
 
 
 class UnetSkipConnectionBlock(nn.Module):
     """Define the Unet submodule with skip connection."""
-    def __init__(self, outer_nc, inner_nc, input_nc=None, submodule=None, outermost=False, innermost=False, norm_layer=nn.BatchNorm2d, use_dropout=False, output_function='sigmoid'):
+    def __init__(self, outer_nc, inner_nc, input_nc=None, submodule=None, outermost=False, innermost=False, norm_layer=nn.BatchNorm2d, use_dropout=False, output_function='sigmoid', key_len=None, redundance_size=None):
         """Construct a Unet submodule with skip connection.
         
         Parameters:
@@ -89,12 +91,16 @@ class UnetSkipConnectionBlock(nn.Module):
             norm_layer                          -- normalization layer
             use_dropout (bool)                  -- if use dropout layers
             output_function (str)               -- activation function for the outmost layer [sigmoid | tanh]
+            key_len (int)                       -- length of secure key (`None` denotes no key)
+            redundance_size (int)               -- redundance size for secure key by a fully connected layer
         """
         super(UnetSkipConnectionBlock, self).__init__()
-        # `submodulde` is None if and only if this block is an innermost block"
-        # `input_nc` is None if and only if this block is not an outermost block"
+        # `submodulde` is None if and only if this block is an innermost block
+        # `input_nc` is None if and only if this block is not an outermost block
 
         self.outmost = outermost
+        self.redundance_size = redundance_size
+
         if type(norm_layer) == functools.partial:
             use_bias = norm_layer.func != nn.BatchNorm2d
         else:
@@ -102,7 +108,14 @@ class UnetSkipConnectionBlock(nn.Module):
         if input_nc is None:
             input_nc = outer_nc
         
-        downconv = nn.Conv2d(input_nc, inner_nc, kernel_size=4, stride=2, padding=1, bias=use_bias)
+        if key_len is None:
+            downconv = nn.Conv2d(input_nc, inner_nc, kernel_size=4, stride=2, padding=1, bias=use_bias)
+            self.encoder = None
+        else:
+            assert outermost
+            downconv = nn.Conv2d(input_nc*2, inner_nc, kernel_size=4, stride=2, padding=1, bias=use_bias)
+            self.encoder = nn.Linear(key_len, redundance_size**2 * input_nc)
+        
         downrelu = nn.LeakyReLU(0.2, True)  # after Conv2d 
         downnorm = norm_layer(inner_nc)
 
@@ -140,16 +153,22 @@ class UnetSkipConnectionBlock(nn.Module):
         
         self.model = nn.Sequential(*model)
 
-    def forward(self, x):
+    def forward(self, x, k=None):
         if self.outmost:
-            return self.model(x)
+            if k is None:
+                return self.model(x)
+            else:
+                b, c, h, w = x.size()
+                r = self.redundance_size
+                ke = self.encoder(k).view(1, c, r, r).repeat(b, 1, h//r, w//r)
+                return self.model(torch.cat([x, ke], dim=1))
         else:
             return torch.cat([x, self.model(x)], dim=1)  # cat by channel
 
 
 class RevealNet(nn.Module):
     """Create a cnn-based reveal network."""
-    def __init__(self, input_nc, output_nc, nrf=64, norm_type='none', output_function='sigmoid'):
+    def __init__(self, input_nc, output_nc, nrf=64, norm_type='none', output_function='sigmoid', key_len=None, redundance_size=None):
         """Construct a reveal network.
         
         Parameters:
@@ -158,11 +177,20 @@ class RevealNet(nn.Module):
             nrf (int)             -- the number of filters in the last conv layer
             norm_type (str)       -- normalization layer type
             output_function (str) -- activation function for the last layer [sigmoid]
+            key_len (int)         -- length of secure key (`None` denotes no key)
+            redundance_size (int) -- redundance size for secure key by a fully connected layer
         """
         super(RevealNet, self).__init__()
+        self.redundance_size = redundance_size
         # input is (3) * 256 * 256
 
-        self.conv1 = nn.Conv2d(input_nc, nrf, kernel_size=3, stride=1, padding=1)
+        if key_len is None:
+            self.conv1 = nn.Conv2d(input_nc, nrf, kernel_size=3, stride=1, padding=1)
+            self.encoder = None
+        else:
+            self.conv1 = nn.Conv2d(input_nc*2, nrf, kernel_size=3, stride=1, padding=1)
+            self.encoder = nn.Linear(key_len, redundance_size**2 * input_nc)
+
         self.conv2 = nn.Conv2d(nrf, nrf*2, kernel_size=3, stride=1, padding=1)
         self.conv3 = nn.Conv2d(nrf*2, nrf*4, kernel_size=3, stride=1, padding=1)
         self.conv4 = nn.Conv2d(nrf*4, nrf*2, kernel_size=3, stride=1, padding=1)
@@ -172,6 +200,7 @@ class RevealNet(nn.Module):
             self.output = nn.Sigmoid()
         else:
             raise NotImplementedError('activation funciton [%s] is not found' % output_function)
+        
         self.relu = nn.ReLU(True)
 
         self.norm_layer = get_norm_layer(norm_type)
@@ -181,7 +210,13 @@ class RevealNet(nn.Module):
         self.norm4 = self.norm_layer(nrf*2)
         self.norm5 = self.norm_layer(nrf)
 
-    def forward(self, X):
+    def forward(self, X, k=None):
+        if k is not None:
+            b, c, h, w = X.size()
+            r = self.redundance_size
+            ke = self.encoder(k).view(1, c, r, r).repeat(b, 1, h//r, w//r)
+            X = torch.cat([X, ke], dim=1)
+        
         X = self.relu(self.norm1(self.conv1(X)))
         X = self.relu(self.norm2(self.conv2(X)))
         X = self.relu(self.norm3(self.conv3(X)))
